d4d53578637c1eb1753605ba0f64294478b34713
Jeff Free <jefffreelance3@gmail.com>
2022-05-26 00:23:55 -0300

    :boom: rounds added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,10 @@ let time = startingMinutes * 60;
 let restTime = startingMinutesRest * 60;
 let rest = false;
 let isRunning = true
+let totalRounds = 0;
+let actualRound = 1;
+let isFirstRound = true;
+
 
 let buttonPause = document.getElementById("button-pause");
 let buttonStart = document.getElementById("button-start");
@@ -12,6 +16,12 @@ let setMinutesInput = document.getElementById("minutes-input");
 let setSecondsInput = document.getElementById("seconds-input");
 let setMinutesRestInput = document.getElementById("minutes-rest-input");
 let setSecondsRestInput = document.getElementById("seconds-rest-input");
+let setRoundsInput = document.getElementById("round-input");
+let roundsText = document.getElementById("rounds");
+let roundsRestartButton = document.getElementById("rounds-restart");
+
+
+totalRounds != 0 ? roundsText.innerText = `Rounds: ${actualRound}/${totalRounds}` : roundsText.innerText = `Round: ${actualRound} / ∞`
 
 buttonStart.disabled = true;
 
@@ -36,6 +46,9 @@ buttonReset.addEventListener("click", function () {
 	countdownEl.innerText = "00:00";
 });
 
+roundsRestartButton.addEventListener("click", restartRounds)
+
+
 const background = document.querySelector("body");
 const countdownEl = document.querySelector("#countdown");
 const timeoutAudio = document.getElementById("timeout_audio");
@@ -48,6 +61,7 @@ let prueba = setInterval(updateCountdown, 1000);
 function updateCountdown() {
 	if (!rest) {
 		background.style.backgroundColor = "blue";
+		isFirstRound = false;
 		const minutes = Math.floor(time / 60);
 		let seconds = time % 60;
 
@@ -70,10 +84,18 @@ function updateCountdown() {
 		rest = !rest;
 		time = startingMinutes * 60;
 		restTime = startingMinutesRest * 60;
+		updateRoundsText();
 	}
 
 	if (countdownEl.innerText === "0:03") {
 		timeoutAudio.play();
+	}
+}
+
+function updateRoundsText(){
+	if(!rest && !isFirstRound){
+		actualRound++;
+		totalRounds != 0 ? roundsText.innerText = `Round ${actualRound} / ${totalRounds}` : roundsText.innerText = `Round: ${actualRound} / ∞`;
 	}
 }
 
@@ -130,6 +152,26 @@ function setRestSeconds(seconds) {
 function setRestSession() {
 	startingMinutesRest = parseInt(setMinutesRestInput.value) + parseInt(setSecondsRestInput.value) / 60;
 	rest = true;
+	isFirstRound = true;
 	buttonReset.click();
+}
+
+function restartRounds(){
+	actualRound = 1;
+	totalRounds != 0 ? roundsText.innerText = `Rounds: ${actualRound}/${totalRounds}` : roundsText.innerText = `Round: ${actualRound} / ∞`
+}
+
+function setRounds(quantity){
+	newValue = parseInt(setRoundsInput.value) + quantity;
+	if (newValue < 0) {
+		setRoundsInput.value = 0;
+	} else {
+		setRoundsInput.value = newValue;
+	}
+}
+
+function setSessionRounds(){
+	totalRounds = parseInt(setRoundsInput.value);
+	totalRounds != 0 ? roundsText.innerText = `Rounds: ${actualRound}/${totalRounds}` : roundsText.innerText = `Round: ${actualRound} / ∞`
 }
 
